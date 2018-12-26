bf1c45226aae0be4f88d63e87d5458ae0fba5edc
萧大侠 <794084226>
2018-12-26 17:47:32 +0800

    a

--- ws/src/main/java/org/xmm/netty/officialdemo/websocketx/server/WebSocketFrameHandler.java
+++ ws/src/main/java/org/xmm/netty/officialdemo/websocketx/server/WebSocketFrameHandler.java
@@ -17,15 +17,22 @@ package org.xmm.netty.officialdemo.websocketx.server;
 
 import java.util.Locale;
 
+import io.netty.channel.Channel;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
 import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketFrame;
+import io.netty.util.concurrent.GlobalEventExecutor;
 
 /**
  * Echoes uppercase content of text frames.
  */
 public class WebSocketFrameHandler extends SimpleChannelInboundHandler<WebSocketFrame> {
+
+    public static ChannelGroup channels = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+
 
     @Override
     protected void channelRead0(ChannelHandlerContext ctx, WebSocketFrame frame) throws Exception {
@@ -42,5 +49,48 @@ public class WebSocketFrameHandler extends SimpleChannelInboundHandler<WebSocket
             String message = "unsupported frame type: " + frame.getClass().getName();
             throw new UnsupportedOperationException(message);
         }
+    }
+
+
+    @Override
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {  // (2)
+        Channel incoming = ctx.channel();
+        for (Channel channel : channels) {
+            channel.writeAndFlush(new TextWebSocketFrame("[SERVER] - " + incoming.remoteAddress() + " 加入"));
+        }
+        channels.add(ctx.channel());
+        System.out.println("Client:"+incoming.remoteAddress() +"加入");
+    }
+
+    @Override
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {  // (3)
+        Channel incoming = ctx.channel();
+        for (Channel channel : channels) {
+            channel.writeAndFlush(new TextWebSocketFrame("[SERVER] - " + incoming.remoteAddress() + " 离开"));
+        }
+        System.out.println("Client:"+incoming.remoteAddress() +"离开");
+        channels.remove(ctx.channel());
+    }
+
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception { // (5)
+        Channel incoming = ctx.channel();
+        System.out.println("Client:"+incoming.remoteAddress()+"在线");
+    }
+
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception { // (6)
+        Channel incoming = ctx.channel();
+        System.out.println("Client:"+incoming.remoteAddress()+"掉线");
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause)
+            throws Exception {
+        Channel incoming = ctx.channel();
+        System.out.println("Client:"+incoming.remoteAddress()+"异常");
+        // 当出现异常就关闭连接
+        cause.printStackTrace();
+        ctx.close();
     }
 }
--- ws/src/main/java/org/xmm/netty/officialdemo/websocketx/server/WebSocketServerInitializer.java
+++ ws/src/main/java/org/xmm/netty/officialdemo/websocketx/server/WebSocketServerInitializer.java
@@ -44,7 +44,7 @@ public class WebSocketServerInitializer extends ChannelInitializer<SocketChannel
         if (sslCtx != null) {
             pipeline.addLast(sslCtx.newHandler(ch.alloc()));
         }
-        pipeline.addLast(new HttpServerCodec());
+        pipeline.addLast("",new HttpServerCodec());
         pipeline.addLast(new HttpObjectAggregator(65536));
         pipeline.addLast(new WebSocketServerCompressionHandler());
         pipeline.addLast(new WebSocketServerProtocolHandler(WEBSOCKET_PATH, null, true));
